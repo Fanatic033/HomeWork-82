5dbe5580628616cf33226037f235709aa36096a0
ilzat <ilzat.sadykov.2006@bk.ru>
2024-09-03 02:28:03 +0600

    Сделал доп задание

--- spotify-backend/routers/track.ts
+++ spotify-backend/routers/track.ts
@@ -2,22 +2,32 @@ import express from 'express';
 import Track from '../Models/Track';
 import {TrackMutation} from '../types';
 import mongoose from 'mongoose';
+import Album from '../Models/Album';
 
 
 const trackRouter = express.Router();
 
 trackRouter.get('/', async (req: express.Request, res: express.Response, next: express.NextFunction) => {
   try {
-    const {album} = req.query;
+    const {album, artist} = req.query;
     let tracks;
-    if (album) {
-      tracks = await Track.find({album: album}).populate('album',);
+
+    if (artist) {
+      const albums = await Album.find({artist: artist}).select('_id');
+      const albumId = albums.map(album => album._id);
+      tracks = await Track.find({album: {$in: albumId}}).populate({
+        path: 'album',
+        populate: {path: 'artist', select: 'title'}
+      });
+    } else if (album) {
+      tracks = await Track.find({album: album}).populate('album');
     } else {
-      tracks = await Track.find()
+      tracks = await Track.find().populate('album');
     }
+
     return res.send(tracks);
   } catch (e) {
-    return next(e)
+    return next(e);
   }
 });
 
